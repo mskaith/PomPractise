c8b0413bff9462141fdcc67e6489061906ab2031
techbrain <tbs.mohitk@gmail.com>
2022-06-15 14:53:34 +0530

    First commit

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+import base.BasePage;
+
+public class LoginPage extends BasePage{
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
